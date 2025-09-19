4143d960caa528e474aed2771fe4ba222ae51315
admin <admin@admins-MacBook-Pro.local>
2025-09-19 16:20:19 +0200

    person and bus

--- Person.py
+++ Person.py
@@ -1,1 +1,12 @@
-Person.py
+
+class Persona:
+    def __init__(self, pnombre, pedad):
+        self.__nombre = pnombre
+        self.__edad = pedad
+    
+    def gettting(self):
+        return f"Nombre: {self.nombre}, Edad: {self.edad}"
+    def setting(self, pnombre, pedad):
+        self.__nombre = pnombre
+        self.__edad = pedad
+#end class Persona
