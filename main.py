5876ae0b2dac23ee345eb63443827966109b79fd
admin <admin@admins-MacBook-Pro.local>
2025-09-25 16:51:33 +0200

    prints y correcion bus

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ from Person import Persona
 from Ticket import Ticket  
 from bus import Bus
 
-plazas = int(input("Ingrese el número de asientos\n"))
+
 plazas_vendidas = 0
-plazas_max = int(plazas)
+
 valor_texto = 0
 vender = True
+
+def plazas_bus():
+    plazas = 0
+    plazas_invalidas = True
+    while  plazas_invalidas :
+        plazas = str(input("Ingrese el número de asientos\n"))
+        if plazas.isdigit():
+            plazas_invalidas = False
+        else:
+            print("\n*******se requiere un numero para poder asignar las plazas*******\n")            
+    return plazas
 
 def devolucion(plazas, plazas_vendidas):
     tickets = int(input())
@@ -30,22 +41,34 @@ def mostrar_menu():
     print("0.- Salir.")
     return
 
-bus_barcelona = Bus(plazas)   
+plazas = plazas_bus()
+plazas_max = int(plazas)
+bus_barcelona = Bus(plazas_max)   
 mostrar_menu()
-
 while vender:
     opcion = input()
+    
     if opcion == "1":
+        print("----Venta de billetes----")
         cliente = Persona(input("Ingrese su nombre\n"), input("Ingrese su apellido\n"))
         venta =  Ticket(cliente)
         print(bus_barcelona.venta_ticket(venta))
+        
     elif opcion == "2":
+        print("----Devolución de billetes----")
         if bus_barcelona.get_asientos_vendidos() == 0:
             print("No hay tickets vendidos")
         else:
             id_ticket=int(input("ingrese id ticket a devolver\n"))
             print(bus_barcelona.devolucion_ticket(id_ticket))
+    
     elif opcion == "3":
+        print("----Estado de la venta----")
         print(bus_barcelona)
+    
     elif opcion == "0":
+        print("----Saliendo del programa----")
         vender = False
+    
+    else:
+        print("====Opción no válida====")
